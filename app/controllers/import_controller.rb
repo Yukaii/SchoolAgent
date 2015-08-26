f3493ea0740d0c90e0843ab22dcb6a02d73c7f66
Yukai Huang <yukaihuang1993@hotmail.com>
2015-08-26 18:57:26 +0800

    Rename app(太過中二), 無意義調整架構

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -3,21 +3,18 @@ class ImportController < ApplicationController
   end
 
   def import_course
-    courses = Agent.fetch({
+    @course_codes = Agent.fetch({
       studentno: params[:studentno],
       idcard: params[:idcard],
       birthday: params[:birthday],
       password: params[:password]
     })
 
-    unless courses.blank?
-      redirect_to import_result_path
+    unless @course_codes.nil?
+      render :result
     else
       redirect_to root_path
     end
   end
 
-  def result
-
-  end
 end
